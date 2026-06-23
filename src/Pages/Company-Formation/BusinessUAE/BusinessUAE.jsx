b3d17859a4396c8bd9221a4dbbaaca7d079b2021
AbdullahAlNoman20 <noman15-5387@diu.edu.bd>
2026-06-23 21:57:12 +0600

    UAE Page Done

--- src/Pages/Company-Formation/BusinessUAE/BusinessUAE.jsx
+++ src/Pages/Company-Formation/BusinessUAE/BusinessUAE.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+
 import BannerUAE from './BannerUAE';
 import Why_Business from './Why_Business';
 import Types_of_Company from './Types_of_Company';
+import BusinessSetupBook from './BusinessSetupBook';
+import ReadyToStublish from './ReadyToStublish';
+import EndToEnd from './EndToEnd';
+import ProcessStep from './ProcessStep';
 
 const BusinessUAE = () => {
     return (
@@ -9,6 +13,11 @@ const BusinessUAE = () => {
             <BannerUAE></BannerUAE>
             <Why_Business></Why_Business>
             <Types_of_Company></Types_of_Company>
+            <ProcessStep></ProcessStep>
+            <EndToEnd></EndToEnd>
+            <ReadyToStublish></ReadyToStublish>
+            <BusinessSetupBook></BusinessSetupBook>
+            
         </div>
     );
 };
