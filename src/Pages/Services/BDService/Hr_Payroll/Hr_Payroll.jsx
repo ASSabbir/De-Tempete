39fcff46097dc4a0f9cd4d4ddacdc18f931558fa
AbdullahAlNoman20 <noman15-5387@diu.edu.bd>
2026-06-25 17:56:03 +0600

    Service page BD Folder Setup

---
+++ src/Pages/Services/BDService/Hr_Payroll/Hr_Payroll.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hr_Payroll = () => {
+    return (
+        <div>
+            <h1>HR & Payroll Solutions</h1>
+        </div>
+    );
+};
+
+export default Hr_Payroll;
