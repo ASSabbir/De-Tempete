bcd07d351d0439b71ce8dcffa59755cabaa7b83c
ASSabbir <atiksabbir1125@gmail.com>
2026-07-24 16:15:42 +0600

    added

--- client/src/api/blogApi.js
+++ client/src/api/blogApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL || 'http://api.detempete.uk/api';
+const API_BASE = import.meta.env.VITE_API_URL || 'https://api.detempete.uk/api';
 
 export const fetchBlogs = (page = 1, limit = 9) =>
   axios.get(`${API_BASE}/blogs`, { params: { page, limit } }).then(res => res.data);
