0ff7665cea2a25c7371b3d0f3629fdde7de4004e
ASSabbir <atiksabbir1125@gmail.com>
2026-07-29 06:05:35 +0600

    added

--- server/routes/Businesssetupleads.js
+++ server/routes/Businesssetupleads.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const BusinessSetupLead = require('../models/businessSetupLeads');
+const BusinessSetupLead = require('../models/Businesssetupleads');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
