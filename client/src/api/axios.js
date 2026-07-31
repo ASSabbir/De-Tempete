0db51cc966fb1f860d6490845b6f4660e8059d93
ASSabbir <atiksabbir1125@gmail.com>
2026-07-31 22:05:41 +0600

    finishing part 1

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://api.detempete.uk/api',
   timeout: 10000,
 });
 
-// http://localhost:5000/api
+// https://api.detempete.uk/api
 
 export default API;
