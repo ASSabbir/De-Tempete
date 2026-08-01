0a16e99fbf697d86ca89a711794e43ef7f464e6c
ASSabbir <atiksabbir1125@gmail.com>
2026-08-01 07:42:46 +0600

    added those section

--- server/routes/Businesssetupleads.js
+++ server/routes/Businesssetupleads.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const BusinessSetupLead = require('../models/businessSetupLeads');
+const BusinessSetupLead = require('../models/businesssetupleads');
 const { protect, authorize } = require('../middleware/auth');
 const { publicLimiter } = require('../middleware/rateLimit');
 
